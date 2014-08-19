eb415e76ff8e4e9eb369a37cb222072c9bdc0c3d
Saravana Kumar <saran87@gmail.com>
2014-08-19 01:32:17 -0400

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Nexmo api config file.
+ *
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Your Nexmo Api Credentials
+    |--------------------------------------------------------------------------
+    |
+    | In order to communicate with an Nexmo service, you must provide your Nexmo
+    | credentials including your Api Key ID and your Api Secret Key.
+    |
+    |
+    */
+    'key'    => '4b25fb68', // Your Nexmo Api Key
+    'secret' => '21c99c33', // Your Nexmo Secret Key
+
+    'from'  => '15853715085'
+];
---
+++ src/Kumar/Nexmo/NexmoServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Kumar\Nexmo;
+
+use Illuminate\Support\ServiceProvider;
+
+class NexmoServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+        $this->app->bindShared('Kumar\\Nexmo\\NexmoClient',function($app){
+
+            $config = $app['config']['nexmo'] ?: $app['config']['nexmo::config'];
+
+            $nexmoMessage =  new NexmoClient($config['key'],$config['secret']);
+
+            $nexmoMessage->setFrom($config['from']);
+
+            return $nexmoMessage;
+        });
+
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
