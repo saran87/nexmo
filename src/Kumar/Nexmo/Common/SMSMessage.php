5627a52edd22becbc6d81f616160e6d109754b12
Saravana Kumar <saran87@gmail.com>
2014-09-20 04:19:54 -0400

    SMSMessage object construction fix

--- src/Kumar/Nexmo/Common/SMSMessage.php
+++ src/Kumar/Nexmo/Common/SMSMessage.php
@@ -5,4 +5,25 @@ namespace Kumar\Nexmo\Common;
 
 class SMSMessage extends SMSObject{
 
+    public function __construct($data = false)
+    {
+        if (!$data) $data = $_REQUEST;
+
+        if (!isset($data['type'])) {
+            throw new NexmoMessageException("Invalid SMSMessage format");
+        }
+
+        parent::__construct($data);
+        // Flag that a receipt was found
+        $this->setAttribute('isExit',true);
+    }
+
+
+    /**
+     * Returns true if a valid receipt is found
+     */
+    public function isText()
+    {
+        return (array_key_exists('type', $this->attributes) &&  $this->attributes['type'] == 'text');
+    }
 }
